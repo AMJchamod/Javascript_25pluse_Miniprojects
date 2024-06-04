ec28be099adda825c5255bcf00e7e23faf15b945
janidu <jchamoth12@gmail.com>
2024-06-04 12:11:48 -0700

    number generator done

--- Dice Roller/main.js
+++ Dice Roller/main.js
@@ -1,9 +1,23 @@
-  const input =document.getElementsByClassName("input").value;
- 
-  const button = document.querySelector("#button");
-  button.addEventListener("click" , dicecontroller);
- function dicecontroller(){
-    alert("dice rolling");
 
-      console.log(input);
- }
+button.addEventListener("click", dicecontroller);
+function dicecontroller() {
+    const input = document.getElementById("input").value;
+    const button = document.getElementById("button");
+    const diceresult =document.getElementById("result");
+    const imgresult = document.getElementById("imgresult");
+    const values = [];
+    const img = [];
+
+    for( let i =0; i<input;i++){
+        const value = Math.floor(Math.random()*6)+1;
+         values.push(value);
+         img.push('<img src="img/${value}.png"');
+    
+       
+    }
+
+    diceresult.textContent = `dice: ${values.join(',')}`;
+ 
+
+    
+}
